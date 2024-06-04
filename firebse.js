71be90b428e2cea13450369f0369e0bdec4e2131
unicorn1231-crt <84387445+unicorn1231-crt@users.noreply.github.com>
2024-06-04 21:52:26 +0900

    Update firebse.js

--- firebse.js
+++ firebse.js
@@ -1,4 +1,11 @@
-// Firebaseの設定を初期化
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBo1mKX9t72bDKyCWvI8oQ8K_G9kcWkdMw",
   authDomain: "visions-60dd7.firebaseapp.com",
@@ -9,12 +16,6 @@ const firebaseConfig = {
   measurementId: "G-97GNLFRSF4"
 };
 
-// Firebaseの初期化
-if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-} else {
-    firebase.app();
-}
-
-const auth = firebase.auth();
-const db = firebase.database();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
