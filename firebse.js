f353a58be88dae2e931534f0734d39c1d90474da
unicorn1231-crt <84387445+unicorn1231-crt@users.noreply.github.com>
2024-06-04 22:10:51 +0900

    Update firebse.js

--- firebse.js
+++ firebse.js
@@ -18,6 +18,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+
 const auth = firebase.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
